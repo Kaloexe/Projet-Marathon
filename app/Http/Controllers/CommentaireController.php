d7c380db5c1c561c4773f0d98bbc7e1f4f8fc745
Sasha Voiseux <sasha_voiseux@ens.univ-artois.fr>
2020-12-17 14:08:31 +0100

    Ajout de la fonction supprimer un commentaire, créations des routes

---
+++ app/Http/Controllers/CommentaireController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Commentaire;
+use Illuminate\Http\Request;
+
+class CommentaireController extends Controller
+{
+    public function show(Request $request, $id) {
+        $action = $request->query('action', 'show');
+        $commentaire = Commentaire::find($id);
+
+        return view('commentaires.show', ['commentaire' => $commentaire, 'action' => $action]);
+    }
+
+    public function delete(Request $request, $id) {
+        $action = $request->get('action','annule');
+        $commentaire = Commentaire::find($id);
+        $jeu_id = $commentaire->jeu->id;
+        if ($action=='valide') {
+            $commentaire->delete();
+        }
+        return redirect()->route('jeu_show',['id' => $jeu_id]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,8 @@ Route::get('/dashboard',"\App\Http\Controllers\JeuController@randomGames");
 
 Route::post('/commentaire/store',"\App\Http\Controllers\AddController@commentairestore")->name('commentaire.store')->middleware('auth');
 Route::get('/tri', [JeuController::class, 'tri'])->name('listeTri');
+Route::get('/commentaire/{id}',[\App\Http\Controllers\CommentaireController::class, 'show'])->name('commentaire.show');
+Route::get('/commentaire/delete/{id}',[\App\Http\Controllers\CommentaireController::class, 'delete'])->name('commentaire.delete');
 
 Route::middleware(['auth:sanctum', 'verified'])->resource('jeu.listeJeux', 'App\Http\Controllers\JeuController');
 Route::get('/jeu/{id}', [JeuController::class, 'show'])->name('jeu_show');
@@ -52,7 +54,6 @@ Route::post('/formulaire', [JeuController::class, 'store'])->name('jeu_store')->
 Route::get('/profil', [ProfilController::class,'index'])->name('profil')->middleware('auth');
 Route::get('/achatjeu', [ProfilController::class,'addAchat'])->name('achatjeu')->middleware('auth');
 Route::get('/supprimer/{id}', [ProfilController::class,'removeAchat'])->name('supprimerA')->middleware('auth');
-
 
 Route::get('/afficheachat', [ProfilController::class,'afficheAchat'])->name('afficheachat')->middleware('auth');
 
