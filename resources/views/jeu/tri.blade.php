d149fb92da64e17e7043411aee368e9dd59aa2ea
Mathieu <mathieumaes182@gmail.com>
2020-12-17 10:59:05 +0100

    tri theme dans page triée

--- resources/views/jeu/tri.blade.php
+++ resources/views/jeu/tri.blade.php
@@ -65,18 +65,38 @@
             <a href="{{'listeJeux'}}">
                 <button class="bg-white text-red-500 px-4 py-2 rounded mr-auto hover:underline">Tri</button>
             </a>
-            <div class="card-body">
-                <form name="form-create-jeu" method="get" action="{{ URL::route('listeJeux') }}">
-                    <div class="form-group">
-                        <label for="description">Editeur</label>
-                        <select name="editeur">
-                            @foreach( \App\Models\Editeur::all() as $editeur)
-                                {{ $id= old('editeur')}}
-                                @if (old('editeur') == $editeur->id)
+            {{--            <div class="card-body">--}}
+            {{--                <form name="form-create-jeu" method="get" action="{{ URL::route('listeJeux') }}">--}}
+            {{--                    <div class="form-group">--}}
+            {{--                        <label for="description">Editeur</label>--}}
+            {{--                        <select name="editeur">--}}
+            {{--                            @foreach( \App\Models\Editeur::all() as $editeur)--}}
+            {{--                                {{ $id= old('editeur')}}--}}
+            {{--                                @if (old('editeur') == $editeur->id)--}}
 
-                                    <option value="{{ $editeur->id }}" selected>{{ $editeur->nom }}</option>
+            {{--                                    <option value="{{ $editeur->id }}" selected>{{ $editeur->nom }}</option>--}}
+            {{--                                @else--}}
+            {{--                                    <option value="{{ $editeur->id }}">{{ $editeur->nom }}</option>--}}
+            {{--                                @endif--}}
+            {{--                            @endforeach--}}
+            {{--                        </select>--}}
+            {{--                    </div>--}}
+            {{--                    <button type="submit" class="btn btn-primary">Submit</button>--}}
+            {{--                </form>--}}
+            {{--            </div>--}}
+
+            <div class="card-body">
+                <form name="form-create-jeu" method="get" action="{{ URL::route('listeJeuxTheme') }}">
+                    <div class="form-group">
+                        <label for="description">Thèmes</label>
+                        <select name="theme">
+                            @foreach( \App\Models\Theme::all() as $theme)
+                                {{ $id= old('theme')}}
+                                @if (old('theme') == $theme->id)
+
+                                    <option value="{{ $theme->id }}" selected>{{ $theme->nom }}</option>
                                 @else
-                                    <option value="{{ $editeur->id }}">{{ $editeur->nom }}</option>
+                                    <option value="{{ $theme->id }}">{{ $theme->nom }}</option>
                                 @endif
                             @endforeach
                         </select>
