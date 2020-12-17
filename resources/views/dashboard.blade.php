7e42f7301f53ca895c0315c7d373b31a76902be8
Mathieu <mathieumaes182@gmail.com>
2020-12-17 18:23:30 +0100

    fix last bug

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -60,7 +60,7 @@
 </header>
 
 <main>
-    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center">
+    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center" >
         <div class="col-md-5 p-lg-5 mx-auto my-5">
             <h1 class="display-4 fw-normal">Bienvenue dans notre boutique de jeux !</h1>
             <p class="lead fw-normal">Grâce aux boutons ci-dessous, vous pouvez choisir 5 jeux aléatoires ou les 5 meilleurs jeux</p>
